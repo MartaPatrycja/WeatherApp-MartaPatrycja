b4abcbae31e8dff4a636c12a52e6695e6ddceb5b
MartaPatrycjaSliwinska <marta.patrycja.web.dev@gmail.com>
2022-05-26 18:32:18 +0200

    updated units to metric only

--- src/index.js
+++ src/index.js
@@ -197,33 +197,7 @@ event.preventDefault();
 navigator.geolocation.getCurrentPosition(searchCurrentLocation);
   }
   
-  function displayFahrenheitTemperature(event) {
-    event.preventDefault();
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-    let  temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-  }
-
-  function displayCelsiusTemperature(event) {
-    event.preventDefault();
-    celsiusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-    let  temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  }
-
-  let fahrenheitLink = document.querySelector("#fahrenheit");
-  fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-  let celsiusLink = document.querySelector("#celsius");
-  celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-
-  let celsiusTemperature = null;
-
-
+  
   let searchForm = document.querySelector("#form-city-button");
   searchForm.addEventListener("submit", submit);
   
